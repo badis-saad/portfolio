22759f1aa483b88bb859c93995628393936f2014
badis <badis.saad@etu.umontpellier.fr>
2026-01-19 15:13:54 +0100

    fix : syntaxe

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from "@/views/Home.vue";
-*import Resume from "@/views/Resume.vue";
+import Resume from "@/views/Resume.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
